1bc7ec877e8e363824bbf7b0aa05a2fd99a8995e
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-16 21:06:12 +0530

    Selection Sort Algorithm

---
+++ selection_sort_algo.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[])
+{
+    for(int i=0;i<4;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<5;j++)
+        {
+            if(arr[j]<arr[min])
+                min=j;
+        }
+        if(min!=i)
+        {
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
+
+int main()
+{
+    int myarr[5];
+    cout << "Enter 5 integer in random order"<< endl;
+    for(int i=0;i<4;i++)
+    {
+        cin >> myarr[i];
+    }
+    cout << "UNSORTED ARRAY"<< endl;
+    for(int i=0;i<4;i++)
+    {
+        cout << myarr[i] << " ";
+    }
+    cout << endl;
+    selectionSort(myarr);
+    cout << "SORTED ARRAY"<< endl;
+    for(int i=0;i<4;i++)
+    {
+        cout << myarr[i] << " ";
+    }
+
+    return 0;
+}
